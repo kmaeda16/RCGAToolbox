b82170a74030b7f42be54d72a025afb5098a3898
maeda <tutta.forza.1618@gmail.com>
2018-12-18 14:57:02 +0900

    Well, I will use decodingfun again.

--- source/Shared/Misc/getFitness.m
+++ source/Shared/Misc/getFitness.m
@@ -1,23 +1,26 @@
 function [f, g, phi] = getFitness(Param,chrom)
 
 n_constraint = Param.n_constraint;
-decodingfun = Param.decodingfun;
 fitnessfun = Param.fitnessfun;
-
+decodingfun = Param.decodingfun;
 x = decodingfun(chrom.gene);
 
-if Param.n_constraint <= 0
-    f = fitnessfun(x);
-    g = 0;
-    phi = 0;
-    if nargout(fitnessfun) >= 2
-        error('%s seems to have f and g as outputs, but g is not used because n_constraint was set to %d.',func2str(fitnessfun),n_constraint);
-    end
-else
-    [f, g] = fitnessfun(x);
-    phi = sum( max(0,g) .^ 2 );
-    length_g = length(g);
-    if n_constraint ~= length_g
-        error('n_constraint was set to %d, but the length of g returned from %s is %d.',n_constraint,func2str(fitnessfun),length_g);
-    end
+n_output = nargout(fitnessfun);
+switch n_output
+    case 1
+        f = feval(fitnessfun,x);
+        g = 0;
+        phi = 0;
+        if n_constraint ~= 0
+            error('n_constraint was set to %d, but %s does not return g.',n_constraint,func2str(fitnessfun));
+        end
+    case 2
+        [f, g] = feval(fitnessfun,x);
+        phi = sum( max(0,g) .^ 2 );
+        length_g = length(g);
+        if n_constraint ~= length_g
+            error('n_constraint was set to %d, but the length of g returned from %s is %d.',n_constraint,func2str(fitnessfun),length_g);
+        end
+    otherwise
+        error('fitnessfun should have one or two output arguments.');
 end
